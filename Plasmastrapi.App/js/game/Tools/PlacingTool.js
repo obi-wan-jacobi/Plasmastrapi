a166dd5aeb5f2770a07c14d3c12912df77fdd63e
Jacob Lichty <jbflicht@gmail.com>
2017-01-29 02:03:37 -0500

    Smoothen placing tool UX.

--- Plasmastrapi.App/js/game/Tools/PlacingTool.js
+++ Plasmastrapi.App/js/game/Tools/PlacingTool.js
@@ -5,18 +5,25 @@
     function PlacingTool() {
         Tool.call(this);
         this.__equippedEntity = null;
+        this.__previousCursorPosition = null
     };
     PlacingTool.prototype.__onequip = function (entity) {
         this.filterByCompatibility($Traits.DesignZone);
         this.__equippedEntity = entity;
+        this.__previousCursorPosition = null;
     };
     PlacingTool.prototype.__input_onmousemove = function (cursor) {
+        if (!this.__previousCursorPosition) {
+            this.__previousCursorPosition = cursor;
+            return;
+        }
         var poseComponent = this.__equippedEntity.getComponent($.PoseComponent)
         var position = poseComponent.position;
         poseComponent.position = new Geometry.Position(
-			cursor.x,
-			cursor.y
+			position.x + (cursor.x - this.__previousCursorPosition.x),
+			position.y + (cursor.y - this.__previousCursorPosition.y)
 		);
+        this.__previousCursorPosition = cursor;
     };
     PlacingTool.prototype.__input_onmouseup = function (cursor) {
         this.__equippedEntity.getComponent($.PickableComponent).deselect();

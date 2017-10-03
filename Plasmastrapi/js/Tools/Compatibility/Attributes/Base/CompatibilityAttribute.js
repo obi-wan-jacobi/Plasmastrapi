cffa8db0185ea4e0c4c61803cb7860043ae6253e
Jacob Lichty <jacob.lichty@hotmail.com>
2017-10-02 23:07:25 -0400

    Stash.

---
+++ Plasmastrapi/js/Tools/Compatibility/Attributes/Base/CompatibilityAttribute.js
@@ -0,0 +1,21 @@
+﻿define(['tool-compatibility-component'],
+function (ToolCompatibilityComponent) {
+
+    function CompatibilityAttribute() {
+        
+    };
+    CompatibilityAttribute.prototype.decorate = function (toolCompatibilityComponent) {
+        if (!(toolCompatibilityComponent instanceof ToolCompatibilityComponent)) {
+            validator.throw(this, this.prototype.name, this.prototype.name + ' may only be applied to arguments of type ' + ToolCompatibilityComponent.name);
+        }
+        Object.defineProperties(toolCompatibilityComponent, {
+            ['is' + this.prototype.name]: {
+                get: function () {
+                    return true;
+                }
+            }
+        });
+    };
+
+    return CompatibilityAttribute;
+});

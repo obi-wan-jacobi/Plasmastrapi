36f147a987a544c7773da957bc26a7cf4a3117b8
LICHTYJ <jacob.lichty@hotmail.com>
2017-11-09 14:21:49 -0500

    Rename tools to lab.

--- Plasmastrapi.App/js/game/InputHandlers/Base/InputHandler.js
+++ Plasmastrapi.App/js/game/InputHandlers/Base/InputHandler.js
@@ -1,5 +1,5 @@
-﻿define(['base', 'utils'],
-function (Base, utils) {
+﻿define(['base', 'validator'],
+function (Base, validator) {
 
     InputHandler.prototype = Object.create(Base.prototype);
     InputHandler.prototype.constructor = InputHandler;
@@ -11,31 +11,44 @@ function (Base, utils) {
     };
     // private methods
     InputHandler.prototype.__oninit = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.__onload = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.__onunload = function () {
+        validator.throwMethodMustBeOverridden();
     };
     // public methods
     InputHandler.prototype.onkeydown = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onkeyup = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onkeypress = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onenter = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onescape = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onmousemove = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onmousedown = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onmouseup = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.onclick = function () {
+        validator.throwMethodMustBeOverridden();
     };
     InputHandler.prototype.dispose = function () {
+        validator.throwMethodMustBeOverridden();
     };
 
     return InputHandler;
---
+++ Plasmastrapi.App/js/lab/Configuration/require.config.js
@@ -0,0 +1,17 @@
+﻿define(function () {
+    var paths = {
+        // Attributes
+        'compatibility-attribute': './lab/Compatibility/Attributes/Base/CompatibilityAttribute',
+        // Compatibility Filters
+        'compatibility-filter': './lab/Compatibility/Filters/Base/CompatibilityFilter',
+        // Input Handlers
+        'placing-tool': './lab/InputHandlers/PlacingTool',
+        // Factories
+        'helper-factory': './lab/Factories/HelperFactory',
+        // Helpers
+        'hit-box': './lab/Helpers/HitBox',
+        'selection-box': './lab/Helpers/SelectionBox'
+    };
+    require.config({ paths: paths });
+    return paths;
+});

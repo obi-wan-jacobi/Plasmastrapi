691adea1095c4b2dadc6f80872a0d716be08e6fc
Jacob Lichty <jacob.lichty@hotmail.com>
2017-11-16 18:26:23 -0500

    Update TODO.

--- Plasmastrapi.App/js/core/Data/Base/DisplaySettings.js
+++ Plasmastrapi.App/js/core/Data/Base/DisplaySettings.js
@@ -1,7 +1,8 @@
-﻿define(['core-constants', 'validator'], function (CORE, validator) {
+﻿define(['core-constants', 'validator'],
+function (constants, validator) {
 
     function DisplaySettings(displayLayer) {
-        if (!(CORE.DISPLAY_LAYERS.indexOf(displayLayer) > -1)) {
+        if (!(constants.DISPLAY_LAYERS.indexOf(displayLayer) > -1)) {
             validator.throw(this, 'constructor', `Display configurations missing for display layer: ${displayLayer}`);
         }
         this.displayLayer = displayLayer;
